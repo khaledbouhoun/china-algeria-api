f30a8f9ed474d90bc3bccb70293a1f337764438f
BOUHOUN KHALED <108300011+khaledbouhoun@users.noreply.github.com>
2026-07-30 16:29:07 +0100

    change welcome view

--- routes/web.php
+++ routes/web.php
@@ -19,35 +19,4 @@ Route::get('/', function () {
 });
 
 
-Route::get('/test-login', function () {
-  return view('test-login');
-});
 
-
-
-Route::get('/firebase-debug', function () {
-  $json = env('FIREBASE_CREDENTIALS_JSON');
-
-  return [
-    'length' => strlen($json),
-    'json_error' => json_last_error_msg(),
-    'decode' => json_decode($json, true),
-
-    'contains_backslash_n' => str_contains($json, '\\n'),
-    'contains_real_newline' => str_contains($json, "-----BEGIN PRIVATE KEY-----\nMII"),
-
-    'first_250' => substr($json, 0, 250),
-
-    'private_key_sample' => preg_match(
-      '/"private_key"\s*:\s*"(.{0,150})/s',
-      $json,
-      $m
-    ) ? $m[1] : null,
-
-    'hex_after_begin' => bin2hex(substr(
-      $json,
-      strpos($json, '-----BEGIN PRIVATE KEY-----'),
-      40
-    )),
-  ];
-});
